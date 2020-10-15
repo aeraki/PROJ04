0128aa49cf926006ee0d26372ff2237174506940
Kishi Aeraki <sluggyben@icloud.com>
2020-10-15 16:06:25 -0400

    Update

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ function pageOnLoad() {
 	if (tag === '') {
 		tag = '#Home';
 		location.hash = '#Home';
-	};
-	if (tag.startsWith('#Article/')) {
+	} else if (tag.startsWith('#Article/')) {
 		let articlename = tag.substring(9);
 		tag='#Article';
 		$('#Article').load('./articles/' + articlename + '.html');
-	};
+	} else if (!$(tag+'.page').length) { tag='#404' };
 	openPage(tag);
 };
 
 function openPage(id) {
 	$('.page').css('display', 'none');
-	$(id).css('display', 'block');
+	$('.page'+id).css('display', 'block');
 };
 
 window.onhashchange = function () {
